b370b6a898be151ca3c43b8605261c845163cc8e
Teje8 <jteruel8@gmail.com>
2022-11-22 20:32:43 -0300

    primeros componentes

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <footer className='container'>
+        <h5 className='footer-h5'>Footer</h5>
+    </footer>
+  )
+}
+
+export default Footer
