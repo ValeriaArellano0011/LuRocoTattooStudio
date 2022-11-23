5ff4cbfa93df02b1214740468e4886f418c88441
Tomi <tomv914@gmail.com>
2022-11-23 14:17:29 -0300

    Creacion main

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import './Main.css'
 
 const Main = () => {
     return (
-        <>
-        </>
+        <main className='container'>
+        <h1 className='main-h1'>Main</h1>
+    </main>
     )
 }
 
