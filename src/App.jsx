7bef2d076f6fe07013840c6017fccec0eb13943f
Teje8 <jteruel8@gmail.com>
2022-11-28 19:35:33 -0300

    arreglo rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
-import Header from './components/header/Header'
-import Footer from './components/footer/Footer'
-import Main from './components/main/Main'
+// import Header from './components/header/Header'
+// import Footer from './components/footer/Footer'
+// import Main from './components/main/Main'
+import Home from './pages/Home'
+import Layout from './layout/Layout'
+import {Routes, Route} from 'react-router-dom'
 
 const App = () => {
   return (
-    <>
-      <Header />
-      <Main />
-      <Footer />
-    </>
+    <div>
+        <Layout>
+          <Routes>
+            <Route path='/' element={<Home/>}/>
+            <></>
+          </Routes>
+        </Layout>
+    </div>
   )
 }
 
