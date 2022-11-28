5e426b94cf368261b0e6a9b4c513e170150aed24
Teje8 <jteruel8@gmail.com>
2022-11-28 19:35:02 -0300

    agrego nav

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import './Header.css'
+import {Link as LinkRouter} from 'react-router-dom'
 
 const Header = () => {
+    let Paginas = [
+        {name: 'Home', to: '/'},
+        {name: 'Artistas', to: '/artistas'},
+        {name: 'Productos', to: '/productos'},
+        {name: 'Sobre nosotrxs', to: '/nosotrxs'},
+        {name: 'Contacto', to: '/contacto'}
+    ]
     return (
-        <header className='container'>
+        <header className='container header'>
             <h1 className='header-h1'>LUROCO TATTOO STUDIO</h1>
+            <div className='Navbar'>
+                {Paginas.map(item =>
+                    <LinkRouter to={item.to}  key={item.key}>{item.name}</LinkRouter>)}
+            </div>
         </header>
     )
 }
