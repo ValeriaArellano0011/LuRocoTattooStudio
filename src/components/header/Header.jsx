a0e519e42aa3943ff2e478f6fbff87e8414fa5c6
Teje8 <jteruel8@gmail.com>
2022-12-02 21:47:57 -0300

    agregado de elementos y estilo header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Header.css'
 import {Link as LinkRouter} from 'react-router-dom'
+import LOGO from '../../assets/LOGO2.png'
 
 const Header = () => {
     let Paginas = [
@@ -11,11 +12,14 @@ const Header = () => {
         {name: 'Contacto', to: '/contacto'}
     ]
     return (
-        <header className='container header'>
-            <h1 className='header-h1'>LUROCO TATTOO STUDIO</h1>
+        <header className='container'>
+            <div className='h1_img'>
+                <h1 className='header-h1'>LUROCO TATTOO STUDIO</h1>
+                <img src={LOGO} alt="logo" className='logo_header'/>
+            </div>
             <div className='Navbar'>
                 {Paginas.map(item =>
-                    <LinkRouter to={item.to}  key={item.key}>{item.name}</LinkRouter>)}
+                    <LinkRouter to={item.to}  key={item.key} className="navbar_links">{item.name}</LinkRouter>)}
             </div>
         </header>
     )
