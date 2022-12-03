4474a54a9817aa8b0cf73fe20c6a4614c3686b43
Teje8 <jteruel8@gmail.com>
2022-12-02 21:47:33 -0300

    agregado estilo y elementos footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
 import './Footer.css'
+import QR from '../../assets/QR.png'
+import {Link as LinkRouter} from 'react-router-dom'
+import {BsInstagram} from 'react-icons/bs'
 
 const Footer = () => {
+
+  let Paginas = [
+    {name: 'Home', to: '/'},
+    {name: 'Artistas', to: '/artistas'},
+    {name: 'Productos', to: '/productos'},
+    {name: 'Sobre nosotrxs', to: '/nosotrxs'},
+    {name: 'Contacto', to: '/contacto'}
+]
   return (
     <footer className='container'>
-        <h1 className='footer-h1'>Footer</h1>
+
+        <div className='footer_Navbar'>
+            {Paginas.map(item =>
+                <LinkRouter to={item.to}  key={item.key} className="navbar_links">{item.name}</LinkRouter>)}
+        </div>
+
+        <div>
+          <small className='text_small'>LUROCO TATTOO STUDIO ©</small>
+          <a href="https://www.instagram.com/luroco.tattostudio/" className='footer_a'><BsInstagram/></a>
+        </div>
+
+        <div className='qr_footer'>
+          <img src={QR} className="qr_img" alt="qr" />
+        </div>
+
     </footer>
   )
 }
