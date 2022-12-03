a9e84d7819c1087877cb86ceb90b418ccab924e4
Teje8 <jteruel8@gmail.com>
2022-12-02 21:55:22 -0300

    nueva pagina artistas

---
+++ src/pages/artistas/Artistas.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Artistas.css'
+
+const Artistas = () => {
+    return (
+        <main className='container'>
+            Artistas
+        </main >
+    )
+}
+
+export default Artistas
