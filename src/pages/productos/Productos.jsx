591d70177a83999a3d12bd2be0e65bdb1dc73aa4
Teje8 <jteruel8@gmail.com>
2022-12-02 21:56:32 -0300

    nueva pagina y estilo productos

---
+++ src/pages/productos/Productos.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Productos.css'
+
+const Productos = () => {
+    return (
+        <main className='container'>
+            Productos
+        </main>
+    )
+}
+
+export default Productos
