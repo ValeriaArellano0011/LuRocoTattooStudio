9dd5287cbc0098cd1bad8816f16f5f06cb3415d5
Teje8 <jteruel8@gmail.com>
2022-12-02 21:56:13 -0300

    estilo y pagina home

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Main from '../../components/main/Main'
+
+const Home = () => {
+    return (
+        <>
+            <Main/>
+        </>
+    )
+}
+
+export default Home
