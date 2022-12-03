52c47663952191908d3830409d202cad6b17b19a
Teje8 <jteruel8@gmail.com>
2022-12-02 21:55:45 -0300

    nueva pagina y estilo contacto

---
+++ src/pages/contacto/Contacto.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Contacto.css'
+
+const Contacto = () => {
+    return (
+        <main className='container'>
+            Contacto
+        </main>
+    )
+}
+
+export default Contacto
