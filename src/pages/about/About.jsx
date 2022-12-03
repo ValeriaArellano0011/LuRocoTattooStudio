53166aec23e4259f754d5d8275b06bfe263e86dd
Teje8 <jteruel8@gmail.com>
2022-12-02 21:55:04 -0300

    nueva pagina about

---
+++ src/pages/about/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './About.css'
+
+const About = () => {
+    return (
+        <main className='container'>
+            About
+        </main>
+    )
+}
+
+export default About
