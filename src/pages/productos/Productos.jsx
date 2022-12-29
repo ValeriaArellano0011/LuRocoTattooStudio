d115679d38eabb35b44c622d6e198d155fd6f6f1
Teje8 <jteruel8@gmail.com>
2022-12-29 00:23:25 -0300

    renderizado dinamico de cards e input search

--- src/pages/productos/Productos.jsx
+++ src/pages/productos/Productos.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useState, useRef }  from 'react'
 import './Productos.css'
+import { useTodosProductosQuery } from '../../features/ProductosApi'
+import ProductosCard from '../../components/productos/ProductosCard'
+import InputSearch from '../../components/InputSearch'
 
 const Productos = () => {
+    const [searchValue,setSearchValue] = useState()
+    const search = useRef(null)
+
+    const handleValue = (e) => {
+        e.preventDefault()
+        setSearchValue(search.current.value)
+    }
+    
+    let { data: productos } = useTodosProductosQuery( searchValue ? searchValue : '')
+
+
     return (
-        <main className='container'>
-            Productos
+        <main className='container productos_main'>
+            <h1 className='productos_h1'>PRODUCTOS</h1>
+            <InputSearch searchEl={search} action={handleValue} />
+            <div className='cards_container'>
+                {productos?.map((item) => {
+                    return (<ProductosCard key={item.key} data={item}/>)
+                })}
+            </div>
         </main>
     )
 }
