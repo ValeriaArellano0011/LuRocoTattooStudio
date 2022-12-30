07d64d66d834a93ac8650ff0ed8811a8ca882b19
Teje8 <jteruel8@gmail.com>
2022-12-30 00:39:06 -0300

    creacion de formulario y estilos al mismo

--- src/pages/contacto/Contacto.jsx
+++ src/pages/contacto/Contacto.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import './Contacto.css'
+import emailjs from '@emailjs/browser'
+
 
 const Contacto = () => {
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs.sendForm('service_6uwz50d', 'template_kip23yg', form.current, '3FlhSFPIm_9WaHCs7')
+        
+        e.target.reset()
+        };
     return (
         <main className='container'>
-            Contacto
+            <h2 className='contacto_h2'>Contactanos</h2>
+            <div>
+                <form ref={form} onSubmit={sendEmail}>
+                    <input type="text" className='input_contacto' name='name' placeholder='Nombre' required/>
+                    <input type="text" className='input_contacto' name='lastName' placeholder='Apellido' required />
+                    <input type="text" className='input_contacto' name='email' placeholder='Email' required/>
+                    <textarea name="message" cols="30" rows="10" placeholder='Tu mensaje...' required></textarea>
+                    <button type='submit' className='btn_contacto'>Enviar</button>
+                </form>
+            </div>
         </main>
     )
 }
