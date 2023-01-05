c54135565335a618c578806266f0a739f947409f
Teje8 <jteruel8@gmail.com>
2023-01-05 20:42:10 -0300

    creacion de componente basico

---
+++ src/pages/registrarse/Registrarse.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Registrarse.css'
+
+const Registrarse = () => {
+    return (
+        <main className='container'>
+            Registrarse
+        </main>
+    )
+}
+
+export default Registrarse
