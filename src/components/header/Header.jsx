4314512d41ad0bdef79e788d7861847e8365e389
Teje8 <jteruel8@gmail.com>
2023-01-05 20:38:06 -0300

    agrego iniciar sesion y registrarse btn

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -15,17 +15,26 @@ const Header = () => {
     ]
 
     return (
-        <header className='container'>
-                <LinkRouter to={'/'} style={{textDecoration: 'none'}}>
-                    <div className='h1_img'>
-                        <h1 className='header-h1'>LUROCO TATTOO STUDIO</h1>
-                        <img src={LOGO} alt="logo" className='logo_header'/>
-                    </div>
-                </LinkRouter>
-                <div className='Navbar'>
-                    {Paginas.map(item =>
-                        <LinkRouter to={item.to}  key={item.key} className="navbar_links">{item.name}</LinkRouter>)}
+        <header>
+            <LinkRouter to={'/'} style={{textDecoration: 'none'}}>
+                <div className='h1_img'>
+                    <h1 className='header-h1'>LUROCO TATTOO STUDIO</h1>
+                    <img src={LOGO} alt="logo" className='logo_header'/>
                 </div>
+            </LinkRouter>
+            <div className='Navbar'>
+                {Paginas.map(item =>
+                    <LinkRouter to={item.to}  key={item.key} className="navbar_links">{item.name}</LinkRouter>)}
+            </div>
+
+            <div className='sesion'>
+                <LinkRouter to={'/registrarse'} style={{textDecoration: 'none'}}>
+                    <p className='registrarse'>Registrarse</p>
+                </LinkRouter>
+                <LinkRouter to={'/iniciarsesion'} style={{textDecoration: 'none'}}>
+                    <p className='iniciar_sesion'>Iniciar Sesion</p>
+                </LinkRouter>
+            </div>
         </header>
     )
 }
