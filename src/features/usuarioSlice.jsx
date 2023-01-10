a9d5c49967e0d6872f12d433b46b49a4a7995423
Teje8 <jteruel8@gmail.com>
2023-01-09 23:15:59 -0300

    configuraciones de usuario para iniciar sesion

---
+++ src/features/usuarioSlice.jsx
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const usuarioSlice = createSlice({
+    name: 'usuario',
+    initialState: {
+        logged: false,
+        usuario: {}
+    },
+    reducers: {
+        logIn: (state) => {
+            state.logged = false
+            state.usuario = {}
+        },
+        logOut: (state) => {
+            state.logged = false
+            state.usuario = {}
+        },
+        setCredentials: (state, action) => {
+            let usuarioData = action.payload.usuario
+            let token = action.payload.token
+            usuarioData ? state.usuario = usuarioData : state.usuario = {}
+            token && localStorage.setItem("token", token)
+        }
+    }
+})
+export const { logIn, logOut, setCredentials } = usuarioSlice.actions
+
+export default usuarioSlice.reducer
