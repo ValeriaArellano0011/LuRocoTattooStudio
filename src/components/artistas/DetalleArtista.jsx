de8741b36655902a5c9787e8ed567377c8e534a4
Teje8 <jteruel8@gmail.com>
2023-01-18 17:42:50 -0300

    agrego tatuajes a artistas y estilo

--- src/components/artistas/DetalleArtista.jsx
+++ src/components/artistas/DetalleArtista.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './ArtistasDetalle.css'
 import { useParams } from 'react-router-dom'
 import { useUnArtistaQuery } from '../../features/ArtistasApi'
+import Tatuajes from '../tatuajes/Tatuajes'
 
 const ArtistaDetalle = () => {
     const {id} = useParams()
@@ -9,11 +10,17 @@ const ArtistaDetalle = () => {
     let { data: artistas } = useUnArtistaQuery(id)
 
     return (
-        <div className='detalle_container'>
-                <img src={artistas?.imagen} alt={artistas?.nombre} className='foto_detalle' />
-                <h2 className='nombre_detalle'>{artistas?.nombre}</h2>
-                <p className='descripcion_detalle'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati sunt consequuntur totam reprehenderit dolores natus, laboriosam numquam ab, quae nisi magni minus a expedita commodi cupiditate architecto. Laboriosam, ducimus animi.</p>
-        </div>
+        <>
+            <div className='detalle_container'>
+                    <img src={artistas?.imagen} alt={artistas?.nombre} className='foto_detalle' />
+                    <h2 className='nombre_detalle'>{artistas?.nombre}</h2>
+                    <p className='descripcion_detalle'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati sunt consequuntur totam reprehenderit dolores natus, laboriosam numquam ab, quae nisi magni minus a expedita commodi cupiditate architecto. Laboriosam, ducimus animi.</p>
+            </div>
+            <h2 className='tatuajes_h2'>Mis trabajos</h2>
+            <div>
+                <Tatuajes artistaId={artistas?.id}/>
+            </div>
+        </>
     )
 }
 
