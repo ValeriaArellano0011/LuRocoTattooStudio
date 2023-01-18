5a3e78960262d916c6ea5c978b37709842812c58
Teje8 <jteruel8@gmail.com>
2023-01-18 17:44:14 -0300

    conexion con api de tatuajes

---
+++ src/features/TatuajesApi.jsx
@@ -0,0 +1,46 @@
+import {createApi,fetchBaseQuery} from '@reduxjs/toolkit/query/react'
+
+
+const TatuajesApi = createApi({
+    reducerPath: "TatuajesApi",
+    
+    baseQuery: fetchBaseQuery({
+        baseUrl: "http://localhost:4000/"
+    }),
+    
+    endpoints: (builder) => ({
+        
+        nuevoTatuaje: builder.mutation({
+            query: (data) => ({
+                url: '/tatuajes',
+                method: "POST",
+                body: data
+            })
+        }),
+
+        tatuajeS: builder.query({
+            query: () => '/tatuajes'
+        }),
+
+        eliminarTatuaje: builder.mutation({
+            query: (id) => ({
+                url: `/tatuajes/${id}`,
+                method: "DELETE",
+                headers: {"Authorization": "Bearer" + localStorage.getItem("token")}
+            })
+        }),
+
+        tatuajeArtista: builder.query({
+            query: (id) => ({
+                url: `/tatuajes/?artistas=${id}`
+            })
+        })
+    })
+})
+
+export default TatuajesApi
+
+export const { useNuevoTatuajeMutation,
+            useTatuajeSQuery,
+            useEliminarTatuajeMutation,
+            useTatuajeArtistaQuery} = TatuajesApi
