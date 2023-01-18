6282bbdeca665b56f691df0458cd7b43cab52d62
Teje8 <jteruel8@gmail.com>
2023-01-18 19:31:25 -0300

    creacion componente registrarse con google

---
+++ src/components/google/RegistrarseGoogle.jsx
@@ -0,0 +1,55 @@
+import React, { useRef, useEffect, useState } from 'react'
+import * as jose from 'jose'
+import { useRegistrarseMutation } from '../../features/UsuariosApi'
+import { useNavigate } from 'react-router-dom'
+import Swal from 'sweetalert2'
+import '../../pages/registrarse/Registrarse.css'
+
+const RegistrarseGoogle = () => {
+    const buttonDiv = useRef()
+    let [newUser, { data: resSignUp, error }] = useRegistrarseMutation()
+    const navigate = useNavigate()
+    
+    async function handleCredentialsResponse (response) {
+        let userObject = jose.decodeJwt(response.credential)
+        let data = {
+            nombre: userObject.given_name,
+            apellido: userObject.family_name,
+            foto: userObject.picture,
+            email: userObject.email,
+            contraseña: userObject.sub,
+            role: 'usuario',
+            from: 'google'
+        }
+        newUser(data)
+        Swal.fire({
+            position: 'top-end',
+            icon: 'success',
+            title: 'Registro exitoso',
+            showConfirmButton: true,
+            timer: 3000
+        })
+        navigate("/iniciarsesion")
+    }
+
+    useEffect(() => {
+        /* global google */
+        google.accounts.id.initialize({
+            client_id: "965988115349-7kg2jiqead2fpbqmpc1juq1u5f5e1eb8.apps.googleusercontent.com",
+            callback: handleCredentialsResponse,
+            context: 'signup'
+        })
+
+        google.accounts.id.renderButton(
+            buttonDiv.current,
+            { theme: "filled_black", size: "large", shape: "rectangular", text: "signup_with" }
+        )
+    }, [])
+    return (
+        <div className='button_google'>
+            <div ref={buttonDiv}></div>
+        </div>
+    )
+}
+
+export default RegistrarseGoogle
