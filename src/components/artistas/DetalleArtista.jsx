174f733593a9fb2410fcf3df7ac5ee50cb46f8fc
Teje8 <jteruel8@gmail.com>
2023-01-25 11:33:28 -0300

    modificacion artistas por artista

--- src/components/artistas/DetalleArtista.jsx
+++ src/components/artistas/DetalleArtista.jsx
@@ -7,18 +7,18 @@ import Tatuajes from '../tatuajes/Tatuajes'
 const ArtistaDetalle = () => {
     const {id} = useParams()
     
-    let { data: artistas } = useUnArtistaQuery(id)
+    let { data: artista } = useUnArtistaQuery(id)
 
     return (
         <>
             <div className='detalle_container'>
-                    <img src={artistas?.imagen} alt={artistas?.nombre} className='foto_detalle' />
-                    <h2 className='nombre_detalle'>{artistas?.nombre}</h2>
+                    <img src={artista?.imagen} alt={artista?.nombre} className='foto_detalle' />
+                    <h2 className='nombre_detalle'>{artista?.nombre}</h2>
                     <p className='descripcion_detalle'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati sunt consequuntur totam reprehenderit dolores natus, laboriosam numquam ab, quae nisi magni minus a expedita commodi cupiditate architecto. Laboriosam, ducimus animi.</p>
             </div>
             <h2 className='tatuajes_h2'>Mis trabajos</h2>
             <div>
-                <Tatuajes artistaId={artistas?.id}/>
+                <Tatuajes artistaId={artista?.id}/>
             </div>
         </>
     )
