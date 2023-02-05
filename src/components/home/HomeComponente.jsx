4b154529c4edbbdd0a67c3b1c848142cf3fe9bef
Teje8 <jteruel8@gmail.com>
2023-02-05 17:28:07 -0300

    home responsive

--- src/components/home/HomeComponente.jsx
+++ src/components/home/HomeComponente.jsx
@@ -1,10 +1,5 @@
 import React from 'react'
 import './HomeComponente.css'
-// import TATTOO1 from '../../assets/tatuaje1.jpg'
-// import TATTOO2 from '../../assets/tatuaje2.jpg'
-// import TATTOO3 from '../../assets/tatuaje3.jpg'
-// import TATTOO4 from '../../assets/tatuaje4.jpg'
-// import TATTOO5 from '../../assets/tatuaje5.jpg'
 import VIDEO_HOME from '../../assets/video_home.mp4'
 import { Link as LinkRouter } from 'react-router-dom'
 
@@ -42,10 +37,11 @@ const Main = () => {
                 </LinkRouter>
             </div>
 
+            <div className='separador'></div>
 
             <div className='mapa'>
-                <h2 className='mapa_h2'>Donde encontrarnos...</h2>
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3350.255655101707!2d-68.84080898427818!3d-32.89140827642533!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x967e0919da344f6f%3A0xaaab5a555c485cc4!2sGaler%C3%ADa%20Piazza%2C%20Av.%20San%20Mart%C3%ADn%201027%2C%20M5500%20Mendoza!5e0!3m2!1ses-419!2sar!4v1669843620340!5m2!1ses-419!2sar" width="600" height="450" style={{ border: 0 }} referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <h2 className='mapa_h2'>Donde encontrarnos 🌎</h2>
+                <iframe className='iframe' title='LuRoco Tattoo Studio' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3350.255655101707!2d-68.84080898427818!3d-32.89140827642533!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x967e0919da344f6f%3A0xaaab5a555c485cc4!2sGaler%C3%ADa%20Piazza%2C%20Av.%20San%20Mart%C3%ADn%201027%2C%20M5500%20Mendoza!5e0!3m2!1ses-419!2sar!4v1669843620340!5m2!1ses-419!2sar" style={{ border: 0 }} referrerPolicy="no-referrer-when-downgrade"></iframe>
             </div>
         </main>
     )
