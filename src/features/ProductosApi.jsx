7358122a02ea4ab61628b4ac1fd310d51d15eda7
Jeremias <jteruel8@gmail.com>
2023-02-05 04:16:09 -0300

    fix problems to deploy

--- src/features/ProductosApi.jsx
+++ src/features/ProductosApi.jsx
@@ -1,5 +1,4 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-require('dotenv').config()
 
 
 const ProductosApi = createApi({
--- src/features/TatuajesApi.jsx
+++ src/features/TatuajesApi.jsx
@@ -1,5 +1,4 @@
 import {createApi,fetchBaseQuery} from '@reduxjs/toolkit/query/react'
-require('dotenv').config()
 
 
 const TatuajesApi = createApi({
