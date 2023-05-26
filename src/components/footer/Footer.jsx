78a21f4d92c0dc280c377ce0a7289866913f3927
Lalo Freak <lalofreak.dev@gmail.com>
2023-05-26 05:30:01 -0400

    to top button component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,26 +1,8 @@
 import React from 'react'
 import './Footer.css'
-import {Link as LinkRouter} from 'react-router-dom'
-import UP_ARROW from '../../assets/up_arrow.png'
 import { SlSocialInstagram } from 'react-icons/sl'
 
 const Footer = () => {
-
-  let Paginas = [
-    {name: 'Home', to: '/'},
-    {name: 'Artistas', to: '/artistas'},
-    {name: 'Productos', to: '/productos'},
-    {name: 'Cursos', to: '/cursos'},
-    {name: 'Sobre nosotrxs', to: '/nosotrxs'},
-    {name: 'Contacto', to: '/contacto'},
-]
-
-const scrollUp = () => {
-    window.scrollTo({
-        top:0,
-        behavior: 'smooth'
-    });
-  }
 
   let date = new Date().getFullYear()
 
@@ -35,9 +17,7 @@ const scrollUp = () => {
           <a href="https://www.instagram.com/luroco.tattostudio/" className='footer_a'>
             <SlSocialInstagram className='instagram_logo'/>
           </a>
-          <button type='button' onClick={scrollUp} className='btn_up_arrow'>
-            <img src={UP_ARROW} alt="up_arrow" className='up_arrow'/>
-          </button>
+
         </div>
 
     </footer>
